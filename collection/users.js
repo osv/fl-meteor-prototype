7112cc594a5403e129429e30078ba91744ec20be
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-01-20 19:22:02 +0200

    user cllection and route

---
+++ collection/users.js
@@ -0,0 +1,59 @@
+Schema = {};
+
+Schema.UserProfile = new SimpleSchema({
+  name: {
+    type: String,
+    optional: true
+  },
+  organization : {
+    type: String,
+    optional: true
+  },
+});
+
+Schema.User = new SimpleSchema({
+  username: {
+    type: String,
+    regEx: /^[a-z0-9A-Z_]{3,15}$/
+  },
+  phone: {
+    type: String,
+    regEx: /^\d{12}$/
+  },
+  emails: {
+    type: [Object],
+    optional: true
+  },
+  "emails.$.address": {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email
+  },
+  "emails.$.verified": {
+    type: Boolean
+  },
+  createdAt: {
+    type: Date
+  },
+  profile: {
+    type: Schema.UserProfile,
+    optional: true
+  },
+  services: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  },
+  // Add `roles` to your schema if you use the meteor-roles package.
+  // Note that when using this package, you must also specify the
+  // `Roles.GLOBAL_GROUP` group whenever you add a user to a role.
+  // Roles.addUsersToRoles(userId, ["admin"], Roles.GLOBAL_GROUP);
+  // You can't mix and match adding with and without a group since
+  // you will fail validation in some cases.
+  roles: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  }
+});
+
+Meteor.users.attachSchema(Schema.User);
