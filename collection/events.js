ff32c4ebf3cc72de02d7fe948555f31defa76d0c
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-02-04 16:49:13 +0200

    Колекция событий
    Будем хранить тут системные события вроде регистрации юзера и тд

---
+++ collection/events.js
@@ -0,0 +1,47 @@
+var eventSchema = new SimpleSchema({
+  createdAt: {
+    type: Date
+  },
+  name: {
+    type: String
+  },
+  description: {
+    type: String,
+    optional: true
+  },
+  important: { // значит не может быть удалено
+    type: Boolean,
+    optional: true
+  }
+});
+
+Events = new Meteor.Collection('events');
+Events.attachSchema(eventSchema);
+
+Events._ensureIndex('createdAt');
+Events._ensureIndex('name');
+
+if (Meteor.isServer) {
+  logEvent = function (event) {
+    if (!_.has(event, 'name')) {
+      throw new Meteor.Error(403, "logEvent without name");
+    }
+    // if event is supposed to be unique, check if it has already been logged
+    if (!!event.unique && !!Events.findOne({name: event.name})) {
+      return;
+    }
+
+    event.createdAt = new Date();
+
+    Events.insert(event);
+
+  };
+
+  Meteor.startup(function () {
+    logEvent({
+      name: "First run",
+      unique: true, // will only get logged a single time 
+      important: true
+    });
+  });
+}
