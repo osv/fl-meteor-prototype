3cae5c4e3ed1d5f43e3cca145c0bb957c6eef91b
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-02-10 18:20:38 +0200

    [server] Методы для смени пароля и логин телефона

--- server/user.js
+++ server/user.js
@@ -25,6 +25,76 @@ Meteor.publish('currentUser', function() {
   return user;
 });
 
+Meteor.methods({
+  // token -  опционально, если undefined, тогда  отправляем SMS, если
+  // же есть, тогда проверяем и меняем номер телефона юзера this.userId
+  'change login phone': function(phone, token) {
+    check(phone, String);
+
+    if (!this.userId)
+      throw new Meteor.Error(401, 'User not logged in');
+
+    // 5ть попыток в 2 минуты только
+    if (!Throttle.checkThenSet('changePhone.' + this.userId, 5, 120000))
+      throw new Meteor.Error(500, 'Превышено количество попыток, подождите немного.');
+
+    var user = Meteor.users.findOne(this.userId, {fields: {services: 1, phone: 1}});
+
+    if (!user)
+      throw new Meteor.Error(500);
+
+    // не позволим поменять телефон на тот который уже есть у другого пользователя
+    // даж если создал он фейк, то пускай поменяет сперва один телефон на другой.
+    if (Meteor.users.findOne({phone: phone}))
+      throw new Meteor.Error(500, 'Ошибка, проверьте номер');
+
+    if (_.isUndefined(token)) {
+      var changeToken = Random.id(6);
+
+      Meteor.users.update(this.userId,
+                          {$set: {'services.change.phone': phone,
+                                  'services.change.token': changeToken}});
+
+      sendSMS("Подтвердите смену телефона на номер: " + 
+              phone + "\n" +
+              "Код: " + changeToken,
+              phone);
+
+      logEvent({type: Events.EV_PROFILE, userId: this.userId, name: "Phone change",
+                desc: "Request for phone change from " + user.phone + " to " + phone});
+
+    } else {
+      if (token !== user.services.change.token)
+        throw new Meteor.Error(403, "Не правильный код, проверьте его.");
+
+      Meteor.users.update( this.userId,
+                           {$set: {
+                             phone: user.services.change.phone },
+                            $unset: { 'services.change': "" }} );
+      logEvent({type: Events.EV_PROFILE, userId: this.userId, name: "Phone change",
+                desc: "Changed phone. Old " + user.phone + ". New " + user.services.change.phone});
+    }
+  },
+  'change password': function(oldPassword, newPassword) {
+    check(oldPassword, String);
+    check(newPassword, String);
+
+    if (!this.userId)
+      throw new Meteor.Error(401, 'User not logged in');
+
+    if (!newPassword.match(/[a-zA-Z0-9+_,./-]{6,}/))
+      throw new Meteor.Error(403, "Пароль должен содержать минимум 6 символов, содержать символы латыни и цифры.");
+
+    var user = Meteor.users.findOne(this.userId, {fields: {password: 1}});
+
+    if (SHA256(oldPassword) === user.password) {
+      Meteor.users.update(this.userId, {$set: {password: SHA256(newPassword)}});
+      logEvent({type: Events.EV_PROFILE, name: "Changed password", userId: this.userId});
+    } else
+      throw new Meteor.Error(403, "Вы ввели не правильный ваш старый пароль.");
+  }
+});
+
 Meteor.startup(function() {
 
   /*  Создадим админа если его нет с паролем admin */
