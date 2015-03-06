ac8b8da712a1b4a86473fb9e137356b724c5eaf8
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-03-07 00:00:16 +0200

    Обновил wSelect (виджет как wInput)

--- client/lib/widgets/selector.js
+++ client/lib/widgets/selector.js
@@ -1,36 +1,112 @@
 /*
 
- Виджет который рисует селект2 и кнопки сохранить, отменить.
+ Виджет селект
 
  Пример:
 
  html:
- {{wSelector context=selectContext
-             placeholder="Select some items}}
+ {{wSelect context=selectContext
+           addonIcon="fa fa-place"
+           placeholder="Select some items}}
  
  js:
 
  Template.sometemplate.helpers({
-  context: function() {
+  selectContext: function() {
     return {
       data: [{id: 'foo1', text: "Txt 2"}, {id: 'foo2', text: "Txt 3", selected: true}],
+
+      getter: 'foo2'; // другой способ установки поумолчанию, вместо selected в data
+
       setter: function(selected) { Messages.info("Selected " + selected);},
-      cancel: function() { Messages.info('Canceled'); },
+
+      placeholder: 'abcd efg'   // другой способ установить плейсхолдер для select2
     };}
 });
 
+ Пример но только формы с кнопками сохранить, отменить:
+
+ html:
+ {{wFormSelect context=selectContext
+           addonIcon="fa fa-place"
+               placeholder="Select some items}}
+ 
+ js:
+
+ Template.sometemplate.helpers({
+  selectContext: function() {
+    return {
+      data: [{id: 'foo1', text: "Txt 2"}, {id: 'foo2', text: "Txt 3", selected: true}],
+
+      getter: 'foo2'; // другой способ установки поумолчанию, вместо selected в data
+
+      setter: function(selected) { Messages.info("Selected " + selected);},
+
+      cancel: function() { Messages.info('Canceled'); },
+
+      placeholder: 'abcd efg'   // другой способ установить плейсхолдер для select2
+    };}
+});
 
 */
 
 function get(v) {
   if (_.isFunction(v))
     return v();
-  else if (_.isString(v))
+  else
     return v;
-  else return null;
 }
 
-Template.wSelector.rendered = function() {
+Template.wSelect.created = function() {
+  this.reactiveVar = new ReactiveVar(); // edit
+};
+
+Template.wSelect.helpers({
+  curvalue: function() {
+    var context = this.context;
+    if (_.isArray(context.data) && context.getter) {
+      var v = _.findDeep(context.data, {id: get(context.getter)});
+      v ? v.text : false;
+    }
+    else
+      return false;
+  },
+
+  msgundef: function() { return this.msgundef || get(this.context.msgundef);},
+
+  undefIcon: function() { return this.undefIcon ||get(this.context.undefIcon);},
+
+  edit: function() {return Template.instance().reactiveVar.get(); },
+
+  // деалем свой контектс, подмешиваем в setter - установку реактивной
+  // переменны, чтобы уйти с режима редактирования. Остальное что нужно, передаем как есть
+  context: function() {
+    var context = this.context,
+        reactiveVar = Template.instance().reactiveVar;
+
+    return {
+      data: context.data,
+      getter: context.getter,
+      placeholder: this.placeholder || get(context.placeholder),
+      addonIcon: this.addonIcon || get(context.placeholder),
+      setter: function(value){
+        reactiveVar.set(false);
+        if (context.setter) {
+          context.setter(value);
+        }
+      },
+      cancel: function() { reactiveVar.set(false); }
+    };
+  }
+});
+
+Template.wSelect.events({
+  'click [data-action="edit"]': function(e, t) {
+    t.reactiveVar.set(true);
+  }
+});
+
+Template.wFormSelect.rendered = function() {
   var context = this.data.context,
         options = {};
 
@@ -39,12 +115,22 @@ Template.wSelector.rendered = function() {
   else if (_.isFunction(context.data))
     options.dataAdapter = context.data;
 
-  options.placeholder = this.data.placeholder || context.placeholder;
+  options.placeholder = this.data.placeholder || get(context.placeholder);
 
-  this.$("select").select2(options);
+  var select = this.$("select");
+
+  select.select2(options);
+
+  // установим значение по умолчанию
+  if (context.getter)
+    select.val( get(context.getter) ).trigger('change');
 };
 
-Template.wSelector.events({
+Template.wFormSelect.helpers({
+  addonIcon: function() { return this.addonIcon || get(this.context.addonIcon);},
+});
+
+Template.wFormSelect.events({
   'click [data-action="save"], submit form': function(e, t) {
     e.preventDefault();
     var context = Template.currentData().context;
@@ -53,7 +139,6 @@ Template.wSelector.events({
 
     if (_.isFunction(context.setter))
       context.setter(value);
-
   },
   'click [data-action="cancel"]': function() {
     var context = Template.currentData().context;
