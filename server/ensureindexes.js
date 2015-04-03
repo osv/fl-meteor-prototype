c0a6c3e0648c1038c063578987ab156b53190fd9
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-04-03 22:36:13 +0300

    Fix unique index (userCats)

--- server/ensureindexes.js
+++ server/ensureindexes.js
@@ -8,6 +8,6 @@
 UserCats._ensureIndex({rm: 1, cat: 1, wrkPlaces: 1, score: -1});
 
 // не позволяем дубликаты
-UserCats._ensureIndex({cat: 1}, {unique: true});
+UserCats._ensureIndex({cat: 1, u: 1}, {unique: true});
 
 UserCats._ensureIndex({u: 1});
